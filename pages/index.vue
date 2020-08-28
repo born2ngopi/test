deef3f34618fbe3847addca4c7a5fc96182f2c77
chandra agung rizky <needkopi@gmail.com>
2020-08-28 08:26:49 +0000

    change photo mockup

--- pages/index.vue
+++ pages/index.vue
@@ -6,19 +6,19 @@
       </div>
     </nav>
     <div
-      class="xl:px-40 sm:px-20 px-10 pt-10 sm:pt-0 lg:row-span-3 grid grid-cols-1 grid-flow-row lg:grid-rows-1 lg:grid-flow-col"
+      class=" xl:px-40 sm:px-20 px-10 pt-10 sm:pt-0 lg:row-span-3 grid grid-cols-1 grid-flow-row lg:grid-rows-1 lg:grid-flow-col"
     >
       <div class="flex flex-col">
         <div class="pt-4 flex flex-col justify-center">
-          <h1 class="sm:text-5xl text-3xl text-white font-bold">
+          <h1 class="sm:text-5xl text-3xl content text-white font-bold">
             One Stop Traveling
           </h1>
-          <h3 class="sm:text-2xl text-lg text-white">
+          <h3 class="sm:text-2xl text-lg text-white content-child">
             solution right on your hand.
           </h3>
         </div>
         <div class="pt-4">
-          <p class="xl:text-xl sm:text-lg text-base font-thin text-white">
+          <p class="xl:text-xl sm:text-lg text-base font-thin text-white content-child">
             Dolin give you flexibility to choose your get-lost-destination.<br />
             Bunch of hidden places you will never imagine to the popular<br />
             destination are here.
@@ -28,21 +28,21 @@
           <img
             src="~/assets/images/comming_on.png"
             alt="link google play"
-            class="cursor-pointer"
+            class="playstore cursor-pointer"
           />
           <img
             src="~/assets/images/soon_on.png"
             alt="link apple store"
-            class="cursor-pointer"
+            class="appstore cursor-pointer"
           />
-          
+
         </div>
         <div></div>
       </div>
       <div class="relative col-span-5">
         <img
           class="tangan"
-          src="~/assets/images/tangan.png"
+          src="~/assets/images/tangan2.png"
           alt="mockup dolin"
         />
       </div>
@@ -75,7 +75,51 @@ export default {};
   background-size: cover;
   background-repeat: no-repeat;
 }
-@media (min-width: 943px) {
+
+@media (max-width:600px) {
+  .playstore{
+    height: 50px;
+    width: 100px;
+  }
+
+  .appstore{
+    height: 50px;
+    width: 100px;
+  }
+}
+
+@media (min-width:1900px){
+  .content{
+    font-size: 7rem;
+  }
+  .content-child{
+    font-size: 2rem;
+  }
+  .tangan{
+    width: 400px;
+  }
+  .awan{
+    height: 5rem;
+  }
+}
+
+@media (min-width:4400px){
+  .content{
+    font-size: 10rem;
+  }
+  .content-child{
+    font-size: 4rem;
+  }
+  .tangan{
+    /* top: 400px; */
+    width: 400px;
+  }
+  .awan{
+    height: 20rem;
+  }
+}
+
+@media (min-width: 984px) {
   .tangan {
     position: absolute;
     bottom: 2rem;
@@ -99,6 +143,7 @@ export default {};
     bottom: 2rem;
     right: 2vw;
     max-width: 30rem;
+    /* width:2000px */
   }
 }
 @media (min-width: 1024px) {
